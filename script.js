50e2fe4614a1d2565286c04336b8f5699d9d1161
kumaradarsh10 <kumar@nextaso.com>
2020-09-21 02:08:10 +0530

    make it better

---
+++ script.js
@@ -0,0 +1,63 @@
+setTimeout(() => {
+  document.getElementById('back').style.display = 'none';
+}, 0);
+
+setTimeout(() => {
+  document.getElementById('header').style.display = 'none';
+}, 0);
+
+setTimeout(() => {
+  document.getElementById('question').style.display = 'none';
+}, 0);
+
+setTimeout(() => {
+  document.getElementById('qus1').style.display = 'none';
+}, 0);
+
+setTimeout(() => {
+  document.getElementById('qus2').style.display = 'none';
+}, 0);
+
+setTimeout(() => {
+  document.getElementById('qus3').style.display = 'none';
+}, 0);
+
+setTimeout(() => {
+  document.getElementById('qus4').style.display = 'none';
+}, 0);
+
+setTimeout(() => {
+  document.getElementById('btn').style.display = 'none';
+}, 0);
+
+setTimeout(() => {
+  document.getElementById('back').style.display = 'block';
+}, 1500);
+
+setTimeout(() => {
+  document.getElementById('header').style.display = 'block';
+}, 2500);
+
+setTimeout(() => {
+  document.getElementById('question').style.display = 'block';
+}, 3500);
+
+setTimeout(() => {
+  document.getElementById('qus1').style.display = 'block';
+}, 4500);
+
+setTimeout(() => {
+  document.getElementById('qus2').style.display = 'block';
+}, 5500);
+
+setTimeout(() => {
+  document.getElementById('qus3').style.display = 'block';
+}, 6500);
+
+setTimeout(() => {
+  document.getElementById('qus4').style.display = 'block';
+}, 7500);
+
+setTimeout(() => {
+  document.getElementById('btn').style.display = 'block';
+}, 8500);
